e7cf2241449aad55b0cca44008f9a965721a0f3a
congibab <engljapen2@gmail.com>
2020-10-10 23:08:26 +0900

    material

--- OpenGL_Third(Lighting)/OpenGL_Third(Lighting)/OpenGL_Third(Lighting).cpp
+++ OpenGL_Third(Lighting)/OpenGL_Third(Lighting)/OpenGL_Third(Lighting).cpp
@@ -37,7 +37,7 @@ float zoom = -3.0f;
 
 GameTime gametime;
 
-glm::vec3 lightPos(1.2f, 1.0f, 1.0f);
+glm::vec3 lightPos(1.2f, 0.5f, 2.0f);
 glm::vec3 cubePos(0.0f, 0.0f, 0.0f);
 
 int main()
@@ -83,7 +83,10 @@ int main()
     glEnable(GL_DEPTH_TEST);
 
 
-	Shader lightingShader("Shader/colors.vs", "Shader/colors.fs");
+	/*Shader lightingShader("Shader/colors.vs", "Shader/colors.fs");
+	Shader lightCubeShader("Shader/light_cube.vs", "Shader/light_cube.fs");*/
+    
+    Shader lightingShader("Shader/Material.vs", "Shader/Material.fs");
 	Shader lightCubeShader("Shader/light_cube.vs", "Shader/light_cube.fs");
 
     //cube vertex
@@ -166,23 +169,31 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         lightingShader.use();
-        lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
-        lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
-        lightingShader.setVec3("lightPos", lightPos);
+        lightingShader.setVec3("light.position", lightPos);
         lightingShader.setVec3("viewPos", camera.Position);
 
-        
+        glm::vec3 lightColor;
+        lightColor.x = sin(glfwGetTime() * 2.0f);
+        lightColor.y = sin(glfwGetTime() * 0.7f);
+        lightColor.z = sin(glfwGetTime() * 1.3f);
+        glm::vec3 diffuseColor = lightColor * glm::vec3(0.5f);
+        glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f);
+        lightingShader.setVec3("light.ambient", ambientColor);
+        lightingShader.setVec3("light.diffuse", diffuseColor);
+        lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+
+        lightingShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
+        lightingShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
+        lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
+        lightingShader.setFloat("material.shininess", 32.0f);
+
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         glm::mat4 view = camera.GetViewMatrix();
         lightingShader.setMat4("projection", projection);
         lightingShader.setMat4("view", view);
         
         glm::mat4 model = glm::mat4(1.0f);
-        //cubePos.x = sin(glfwGetTime());
-        //cubePos.z = sin(glfwGetTime());
-        //model = glm::translate(model, cubePos);
         lightingShader.setMat4("model", model);
-
 
         glBindVertexArray(cubeVAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
@@ -192,6 +203,8 @@ int main()
         lightCubeShader.setMat4("projection", projection);
         lightCubeShader.setMat4("view", view);
         model = glm::mat4(1.0f);
+        lightPos.x = cos(glfwGetTime());
+        lightPos.z = sin(glfwGetTime());
         model = glm::translate(model, lightPos);
         model = glm::scale(model, glm::vec3(0.2f));
         lightCubeShader.setMat4("model", model);
@@ -257,7 +270,10 @@ void key_callback(GLFWwindow* window, int key, int scencode, int action, int mod
 		camera.ProcessKeyboard(RIGHT, gametime.GetDeltaTime());
         break;
 		//=====================
-	default:
+    case GLFW_KEY_R:
+        camera.Position = glm::vec3(0,0,0);
+    
+    default:
 		cout << "Pressed default Key " << endl;
 		break;
 	}
@@ -303,326 +319,3 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 
 
 }
-
-//#include <glad/glad.h>
-//#include <GLFW/glfw3.h>
-//
-//#include <glm/glm.hpp>
-//#include <glm/gtc/matrix_transform.hpp>
-//#include <glm/gtc/type_ptr.hpp>
-//
-//#include <iostream>
-//
-//#include "Camera.h"
-//#include "Shader.h"
-//#include "GameTIme.h"
-//
-//using namespace std;
-//
-//#pragma comment(lib, "glfw3.lib")
-//
-//
-//void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-//void key_callback(GLFWwindow* window, int key, int scencode, int action, int mods);
-//void cursor_position_callback(GLFWwindow* window, double xpos, double ypos);
-//void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
-//void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
-//
-//
-//// settings
-//const unsigned int SCR_WIDTH = 800;
-//const unsigned int SCR_HEIGHT = 600;
-//
-//Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
-//float lastX = SCR_WIDTH / 2.0f;
-//float lastY = SCR_HEIGHT / 2.0f;
-//bool firstMouse = true;
-//
-//float mixValue = 0.2f;
-//float zoom = -3.0f;
-//
-//GameTime gametime;
-//
-//glm::vec3 lightPos(1.2f, 1.0f, 1.0f);
-//
-//int main()
-//{
-//    // glfw: initialize and configure
-//    // ------------------------------
-//    glfwInit();
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-//    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-//
-//#ifdef __APPLE__
-//    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-//#endif
-//
-//    // glfw window creation
-//    // --------------------
-//    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-//    if (window == NULL)
-//    {
-//        std::cout << "Failed to create GLFW window" << std::endl;
-//        glfwTerminate();
-//        return -1;
-//    }
-//    glfwMakeContextCurrent(window);
-//    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-//    glfwSetKeyCallback(window, key_callback);
-//    glfwSetCursorPosCallback(window, cursor_position_callback);
-//    glfwSetScrollCallback(window, scroll_callback);
-//
-//    // tell GLFW to capture our mouse
-//    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-//
-//    // glad: load all OpenGL function pointers
-//    // ---------------------------------------
-//    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-//    {
-//        std::cout << "Failed to initialize GLAD" << std::endl;
-//        return -1;
-//    }
-//
-//    // configure global opengl state
-//    // -----------------------------
-//    glEnable(GL_DEPTH_TEST);
-//
-//    // build and compile our shader zprogram
-//    // ------------------------------------
-//    Shader lightingShader("Shader/colors.vs", "Shader/colors.fs");
-//    Shader lightCubeShader("Shader/light_cube.vs", "Shader/light_cube.fs");
-//
-//    // set up vertex data (and buffer(s)) and configure vertex attributes
-//    // ------------------------------------------------------------------
-//    float vertices[] = {
-//        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-//         0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-//         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-//         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-//        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-//        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-//
-//        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-//         0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-//         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-//         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-//        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-//        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-//
-//        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-//        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-//        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-//        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-//        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-//        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-//
-//         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-//         0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-//         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-//         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-//         0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-//         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-//
-//        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-//         0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-//         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-//         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-//        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-//        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-//
-//        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
-//         0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
-//         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-//         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-//        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-//        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
-//    };
-//    // first, configure the cube's VAO (and VBO)
-//    unsigned int VBO, cubeVAO;
-//    glGenVertexArrays(1, &cubeVAO);
-//    glGenBuffers(1, &VBO);
-//
-//    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-//    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-//
-//    glBindVertexArray(cubeVAO);
-//
-//    // position attribute
-//    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-//    glEnableVertexAttribArray(0);
-//    // normal attribute
-//    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-//    glEnableVertexAttribArray(1);
-//
-//
-//    // second, configure the light's VAO (VBO stays the same; the vertices are the same for the light object which is also a 3D cube)
-//    unsigned int lightCubeVAO;
-//    glGenVertexArrays(1, &lightCubeVAO);
-//    glBindVertexArray(lightCubeVAO);
-//
-//    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-//    // note that we update the lamp's position attribute's stride to reflect the updated buffer data
-//    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-//    glEnableVertexAttribArray(0);
-//
-//
-//    // render loop
-//    // -----------
-//    while (!glfwWindowShouldClose(window))
-//    {
-//        gametime.Time_Measure();
-//        // per-frame time logic
-//        // --------------------
-//        float currentFrame = glfwGetTime();
-//
-//        // input
-//        // -----
-//
-//        // render
-//        // ------
-//        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-//        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-//
-//        // be sure to activate shader when setting uniforms/drawing objects
-//        lightingShader.use();
-//        lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
-//        lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
-//        lightingShader.setVec3("lightPos", lightPos);
-//        lightingShader.setVec3("viewPos", camera.Position);
-//
-//        // view/projection transformations
-//        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-//        glm::mat4 view = camera.GetViewMatrix();
-//        lightingShader.setMat4("projection", projection);
-//        lightingShader.setMat4("view", view);
-//
-//        // world transformation
-//        glm::mat4 model = glm::mat4(1.0f);
-//        lightingShader.setMat4("model", model);
-//
-//        // render the cube
-//        glBindVertexArray(cubeVAO);
-//        glDrawArrays(GL_TRIANGLES, 0, 36);
-//
-//
-//        // also draw the lamp object
-//        lightCubeShader.use();
-//        lightCubeShader.setMat4("projection", projection);
-//        lightCubeShader.setMat4("view", view);
-//        model = glm::mat4(1.0f);
-//        model = glm::translate(model, lightPos);
-//        model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
-//        lightCubeShader.setMat4("model", model);
-//
-//        glBindVertexArray(lightCubeVAO);
-//        glDrawArrays(GL_TRIANGLES, 0, 36);
-//
-//        gametime.DeltaTime_Update();
-//
-//        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-//        // -------------------------------------------------------------------------------
-//        glfwSwapBuffers(window);
-//        glfwPollEvents();
-//    }
-//
-//    // optional: de-allocate all resources once they've outlived their purpose:
-//    // ------------------------------------------------------------------------
-//    glDeleteVertexArrays(1, &cubeVAO);
-//    glDeleteVertexArrays(1, &lightCubeVAO);
-//    glDeleteBuffers(1, &VBO);
-//
-//    // glfw: terminate, clearing all previously allocated GLFW resources.
-//    // ------------------------------------------------------------------
-//    glfwTerminate();
-//    return 0;
-//}
-//
-////CallBack function============================================
-////============================================
-//void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-//{
-//    glViewport(0, 0, width, height);
-//}
-//
-//void key_callback(GLFWwindow* window, int key, int scencode, int action, int mods)
-//{
-//    float CameraSpeed = 5.0f;
-//
-//    cout << camera.Position.x << "," << camera.Position.y << "," << camera.Position.z << endl;
-//    switch (key)
-//    {
-//    case GLFW_KEY_ESCAPE:
-//        glfwSetWindowShouldClose(window, true);
-//        break;
-//    case GLFW_KEY_UP:
-//        mixValue += 0.1f;
-//        break;
-//    case GLFW_KEY_DOWN:
-//        mixValue -= 0.1f;
-//        break;
-//        //=====================
-//        //Camera movement input
-//    case GLFW_KEY_W:
-//        //cameraPos += CameraSpeed * cameraFront * gametime.GetDeltaTime();
-//        camera.ProcessKeyboard(FRORWARD, gametime.GetDeltaTime());
-//        break;
-//    case GLFW_KEY_S:
-//        //cameraPos -= CameraSpeed * cameraFront * gametime.GetDeltaTime();
-//        camera.ProcessKeyboard(BACKWORD, gametime.GetDeltaTime());
-//        break;
-//    case GLFW_KEY_A:
-//        //cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * CameraSpeed * gametime.GetDeltaTime();
-//        camera.ProcessKeyboard(LEFT, gametime.GetDeltaTime());
-//        break;
-//    case GLFW_KEY_D:
-//        //cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * CameraSpeed * gametime.GetDeltaTime();
-//        camera.ProcessKeyboard(RIGHT, gametime.GetDeltaTime());
-//        break;
-//        //=====================
-//    default:
-//        cout << "Pressed default Key " << endl;
-//        break;
-//    }
-//}
-//
-//void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
-//{
-//    //cout << "Mouse Position (" << xpos << "," << ypos << ")" << endl;
-//    if (firstMouse)
-//    {
-//        lastX = xpos;
-//        lastY = ypos;
-//        firstMouse = false;
-//    }
-//
-//    float xoffset = xpos - lastX;
-//    float yoffset = lastY - ypos;
-//
-//    lastX = xpos;
-//    lastY = ypos;
-//
-//    camera.ProcessMouseMovement(xoffset, yoffset);
-//}
-//
-//void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
-//{
-//    //zoom += yoffset;
-//    camera.ProcessMouseScroll(yoffset);
-//    //cout << "Mouse Scroll Offset (" << xoffset << "," << yoffset << ")" << endl;
-//}
-//
-//void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
-//{
-//    //if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
-//    //{
-//    //	cout << "Mouse Clicked right button" << endl;
-//    //}
-//
-//    //if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
-//    //{
-//    //	cout << "Mouse Clicked right left" << endl;
-//    //}
-//
-//
-//}
--- OpenGL_Third(Lighting)/OpenGL_Third(Lighting)/Shader/colors.fs
+++ OpenGL_Third(Lighting)/OpenGL_Third(Lighting)/Shader/colors.fs
@@ -18,6 +18,7 @@ void main()
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
+
     vec3 diffuse = diff * lightColor;
 //=========
  
