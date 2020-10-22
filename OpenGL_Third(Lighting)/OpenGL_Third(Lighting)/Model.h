fb5c59da1aafa5538687b4f9d4cd5485124a91d0
congibab <engljapen2@gmail.com>
2020-10-23 03:37:26 +0900

    nodeling Loard function adding

---
+++ OpenGL_Third(Lighting)/OpenGL_Third(Lighting)/Model.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <glad/glad.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#include <stb_image.h>
+
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <map>
+#include <vector>
+
+#include "Shader.h"
+#include "Mesh.h"
+
+class Model
+{
+public:
+	Model(char* path) { loadModel(path); };
+	void Draw(Shader shader);
+private:
+	std::vector<Mesh> meshes;
+	std::string directory;
+
+	void loadModel(std::string path);
+	void processNode(aiNode *node, const aiScene *scene);
+	Mesh processMesh(aiMesh *mesh, const aiScene *sceme);
+	std::vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, std::string typeName);
+	unsigned int TextureFromFile(const char* path, const std::string &directory, bool gamma);
+};
+
